47b24f857ec4f6a20a53d1398f75004057918f01
Remo Frey <remo.frey@autoidlabs.ch>
2017-05-22 11:52:47 -0400

    Added Params.

--- App.js
+++ App.js
@@ -10,12 +10,38 @@ class HomeScreen extends React.Component {
     title: 'Welcome',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat', { user: 'Remo' })}
+          title="Chat with Remo"
+        />
+      </View>
+    );
   }
 }
 
-const SimpleApp = StackNavigator({
+class ChatScreen extends React.Component {
+  // Nav options can be defined as a function of the screen's props:
+  static navigationOptions = ({ navigation }) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
+  render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user}</Text>
+      </View>
+    );
+  }
+}
+
+const GoWild = StackNavigator({
   Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
 });
 
-AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
+AppRegistry.registerComponent('GoWild', () => GoWild);
